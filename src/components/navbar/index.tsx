7b4a7c69dd04785785abdace91eb6eb1b46b55e4
simpangyo <spg9468@naver.com>
2024-04-23 20:44:46 +0900

    Update Routes Mainpage

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+import "../css/navbar.scss";
+
+function Navbar () {
+    return (
+        <div className="wrap">
+            
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/navbar";
+
+
+function MainPage () {
+    return(
+        <>
+            <Navbar/>
+        </>
+    );
+}
+
+export default MainPage;
